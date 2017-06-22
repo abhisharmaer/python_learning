149572ca0e2a54c0fc4b9b21ebe3a6b07ee550b9
Abhishek Sharma <abhisharma.er@gmail.com>
2017-06-22 16:22:01 +0530

    Add files via upload
    Updated print_demo_list function as overloaded function.

--- demo_lists.py
+++ demo_lists.py
@@ -1,21 +1,34 @@
 #%%
 #This script is to demonstrate Python Lists.
+my_list = ['Mike', 'Trump', '9999999999']   #Defining, initializing a Global List
 
-my_list = ['Mike', 'Trump', '9999999999']   #Defining, initializing List
-print('First Name: ', my_list[0])           #Printing 1st item in list.
-print('Last Name: ', my_list[1])            #Printing 2nd item in list.
-print('Mobile Name: ', my_list[2])          #Printing 3rd item in list.
-print("Length of my list: " , len(my_list))
+#Defining print_demo_list as overloaded function.
+def print_demo_list(comment=None,demolist=None,index=None):      
+    
+    if index is None and demolist is None and comment is None:
+        print("Blank, No Arguments provided")
+    elif index is None and demolist is None and comment is not None:
+        print(comment)
+    elif index is None and demolist is not None:
+        print(comment,demolist)
+    elif index is not None:
+        print(comment,demolist[index])
+   
+#Printing Demo List with different parameters using overloaded function print_demo_list(params..)
+print_demo_list()
+print_demo_list("Printing Comments only")
+print_demo_list('My Demo List: ', my_list)        
+print_demo_list('First Name is: ', my_list, 0)
 
 #Adding a new item in list at a given index.
 my_list.insert(0,'Mr.')
-print('My List with Salutation added at 0 index is: ', my_list)
+print_demo_list('My List with Salutation added at 0 index is: ', my_list)
 
-#Adding a new item with append method.
+##Adding a new item with append method.
 my_list.append(['Male', 'Professional'])
 print('My List with some items appended is: ', my_list)
 
-#Adding a new item with extended method.
+##Adding a new item with extended method.
 my_list.pop(len(my_list) - 1)
 my_list.extend(['Male', 'Professional'])
 print('My List with some items extended is: ', my_list)
