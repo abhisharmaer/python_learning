ade6b54f932e63ecb9a066658bc035fe59da79c7
Abhishek Sharma <abhisharma.er@gmail.com>
2017-06-23 19:33:42 +0530

    Add files via upload

--- demo_lists.py
+++ demo_lists.py
@@ -13,8 +13,8 @@ def print_demo_list(comment=None,demolist=None,index=None):
     elif index is not None:
         print(comment,demolist[index])
    
-#Remove a specific item from list with another using enumerate as iterator.
-def remove_item(searchitem,replaceitem):
+#Replace a specific item from list with another using enumerate as iterator.
+def replace_item(searchitem,replaceitem):
     for i, val in enumerate(my_list):
         if val == searchitem:
             my_list[i] = replaceitem
@@ -24,6 +24,27 @@ def remove_item(searchitem,replaceitem):
         else:
             print_demo_list('My List without any replacement is: ', my_list)
             return my_list
+
+#Removing all occurances of an item in a list.
+def rem_item_occurance(item=None,all_flag=None,index=None):    #TODO: how to have item as a list to remove multiple items at once?
+    if all_flag == True:
+        for i, val in enumerate(my_list2):
+            if val == item:
+                my_list2.remove(val)
+                print("Item", item,"removed from index[",i,"]")
+            else:
+                print("Item",item,"not found in my_list2 @ index[",i,"]" )
+        return(my_list2)
+    else:
+        for i, val in enumerate(my_list2):
+            if val == item:
+                my_list2.pop(index)
+                print("Item found and removed at index[",i,"] \n",my_list2)
+                break
+            else:
+                print("Item to be removed not found at index: [",i,"]")
+        #return(my_list2)
+
 
 #Printing Demo List with different parameters using overloaded function
 #print_demo_list(params..)
@@ -46,7 +67,7 @@ my_list.extend(['Male', 'Professional'])
 print('My List with some items extended is: ', my_list)
 
 #Removing a specific item with another
-print_demo_list(remove_item("Mr.", "Dr."))
+print_demo_list(replace_item("Mr.", "Dr."))
 
 #To concatenate a list with another list.
 copy_list = my_list.copy()
@@ -55,5 +76,10 @@ print_demo_list("List concatenated with reversed copy of my_list is: ",my_list +
 
 #To repeat a list.
 print_demo_list("Printing My List with repeatition: ", 2 * my_list)
+
+my_list2 = 2 * my_list
+rem_item_occurance('Mike',True)
+rem_item_occurance('Trump',False,0)
+rem_item_occurance('Dr.',False,0)
 
 
